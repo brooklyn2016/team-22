bdcf0d5dfd21a3820a76544ffd50500e0ae7cdd5
Jason Deng <jdeng1234@gmail.com>
2016-10-29 02:30:09 -0400

    added issac

--- client/routes.js
+++ client/routes.js
@@ -4,6 +4,7 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import Home from './components/Home';
 import Login from './components/Login';
+import Issac from './components/Issac';
 
 
 export default (
@@ -12,5 +13,6 @@ export default (
       <Route path="/home" component={Home} />
       <Route path="/leaderboard" component={Home} />
       <Route path="/friends" component={Home} />
+      <Route path="/lesson" component={Issac} />
   </Route>
 );
