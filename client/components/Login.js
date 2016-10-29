14c56a50c4b48c7c711b7e08b17c6ae8cf86cad2
Jason Deng <jdeng1234@gmail.com>
2016-10-29 04:13:02 -0400

    added some stuff

--- client/components/Login.js
+++ client/components/Login.js
@@ -3,7 +3,6 @@ import { reduxForm } from 'redux-form';
 import * as actions from '../actions/';
 import { Link, browserHistory } from 'react-router';
 
-
 class Login extends Component {
   handleFormSubmit({ username, password }) {
     // Need to do something to log user in
@@ -16,22 +15,29 @@ class Login extends Component {
     const { handleSubmit, fields: { username, password }} = this.props;
 
     return (
-      <div>
-        <img src="img/victorcruz.png" />
-        <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-          <label>Username</label>
-            <input {...username} type="text" placeholder="Enter username" />
-          <label>Password</label>
-            <input {...password} type="password" placeholder="Enter password" />
-          <button action="submit">Login</button>
-          <button >Sign up</button>
-        </form>
+      <div className="blueGradient">
+        <div className="row">
+          <div className="col-sm-12">
+            <img className="img-responsive" src="img/victorcruz.png" />
+          </div>
+        </div>
+
+            <form className="row" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+            <div className="col-sm-12">
+                <input className="" {...username} type="text" placeholder="Username" />
+              </div>
+              <div className="col-sm-12">
+                <input {...password} type="password" placeholder="Password" />
+              </div>
+                <button className="col-sm-6" action="submit">Login</button>
+                <button className="col-sm-6">Sign up</button>
+            </form>
       </div>
     )
   }
 }
 
 export default reduxForm({
-  form: 'login',
+  form: 'Login',
   fields: ['username', 'password']
 }, null, actions)(Login);
