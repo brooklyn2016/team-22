8b3ed68294b541c14fd9f0589a65c860578f3de2
Cheng <cproripop@gmail.com>
2016-10-29 08:30:39 -0400

    Implement carousel and some styling

--- client/components/Issac.js
+++ client/components/Issac.js
@@ -10,60 +10,59 @@ class Issac extends Component {
 
 	render() {
 		return (
-			<div>
+			<div className="lesson-bg">
 
         <h3 className="display-3">{this.props.lessons.lesson[0].lessonName}</h3>
 
+        <div className="container">
       
-        <div id="myCarousel" className="carousel slide" data-ride="carousel">
-            <ol className="carousel-indicators">
-              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-              <li data-target="#myCarousel" data-slide-to="1"></li>
-              <li data-target="#myCarousel" data-slide-to="2"></li>
-              <li data-target="#myCarousel" data-slide-to="3"></li>
-            </ol>
+          <div id="myCarousel" className="carousel slide" data-ride="carousel">
+              <ol className="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+                <li data-target="#myCarousel" data-slide-to="3"></li>
+              </ol>
 
-            <div className="carousel-inner" role="listbox">
-              <div className="item active">
-              <p className="objective">Objective</p>
-              <p className="content">{this.props.lessons.lesson[0].objective}</p>
+              <div className="carousel-inner" role="listbox">
+                <div className="item active">
+                <p className="objective">Objective</p>
+                <p className="content">{this.props.lessons.lesson[0].objective}</p>
 
+                </div>
+
+                <div className="item">
+                <p>
+                {this.props.lessons.lesson[0].background[0].title}
+                <br />
+
+                {this.props.lessons.lesson[0].background[0].desc}
+                </p>
+
+                </div>
+
+                <div className="item">
+                <p>
+                {this.props.lessons.lesson[0].background[1].title}
+                <br />
+                {this.props.lessons.lesson[0].background[1].desc}
+                </p>
+                <Link to={'/q1'}>
+                  <button className="btn btn-lg btn-success">BEGIN MODULE</button>
+                </Link>
+                </div>
               </div>
 
-              <div className="item">
-              <p>
-              {this.props.lessons.lesson[0].background[0].title}
-              <br />
-
-              {this.props.lessons.lesson[0].background[0].desc}
-              </p>
-
-              </div>
-
-              <div className="item">
-              <p>
-              {this.props.lessons.lesson[0].background[1].title}
-              <br />
-              {this.props.lessons.lesson[0].background[1].desc}
-              </p>
-              </div>
-
-              <div className="item">
-              </div>
+              <a className="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span className="sr-only">Previous</span>
+              </a>
+              <a className="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span className="sr-only">Next</span>
+              </a>
             </div>
-
-            <a className="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-              <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-              <span className="sr-only">Previous</span>
-            </a>
-            <a className="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-              <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-              <span className="sr-only">Next</span>
-            </a>
           </div>
-        <Link to={'/q1'}>
-          <button>Questions</button>
-        </Link>
 
         <FooterNav />
 		 	</div>
