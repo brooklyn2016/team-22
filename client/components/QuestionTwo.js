8ba6d9d2cde3090b966b16f8e143d7266e1fb190
Jason Deng <jdeng1234@gmail.com>
2016-10-29 07:04:31 -0400

    added questions

--- client/components/QuestionTwo.js
+++ client/components/QuestionTwo.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
+import FooterNav from './Footer';
 
 class QuestionTwo extends Component {
   constructor(props) {
     super(props);
-
-    console.log(this.props.lessons.lesson[5]);
+    console.log(this.props.lessons);
   }
 
 	render() {
 		return (
+      // lesson, material, instruct, question, funfact
 			<div>
+      <div>{this.props.lessons.lesson[0].questions[1].lesson}</div>
+      <div>{this.props.lessons.lesson[0].questions[1].materials[0]}</div>
+      <div>{this.props.lessons.lesson[0].questions[1].materials[1]}</div>
+
+      <div>{this.props.lessons.lesson[0].questions[1].instruction}</div>
+      <div>{this.props.lessons.lesson[0].questions[1].question}</div>
+      <div>{this.props.lessons.lesson[0].questions[1].funFact}</div>
+
+
 
         <Link to={'/q3'}>
           <button>Questions </button>
         </Link>
+
+        <FooterNav />
+
 		 	</div>
 		);
 	}
